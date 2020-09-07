11915d7fa3139f75d37fb040bd8ba449919db887
Damian-Borowski <dbcreativeproductions@gmail.com>
2020-09-07 08:20:07 +0200

    Newsletter ExpressJS

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion: ES6;
+
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const port = 5000;
+const https = require('https');
+
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.emailAdress;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/lists/c10a536be4";
+  const options = {
+    method: "POST",
+    auth: "damian1:7448953076b98f8f04aa249065ccc674-us17"
+  }
+  const request = https.request(url, options, (response) => {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", (req, res) => {
+  res.redirected("/");
+})
+
+app.listen(port, () => {
+  console.log('Server is working');
+})
+
+
+// API KEY
+//7448953076b98f8f04aa249065ccc674-us17
+
+//LIST ID:
+//c10a536be4
